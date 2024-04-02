185de5422b7cc3f985b127fefdba338dc6cc7765
truemat <matvei.truhan@gmail.com>
2024-04-02 22:45:55 +0300

    fix import Apache Commons Lang

--- src/main/java/io/hexlet/example/App.java
+++ src/main/java/io/hexlet/example/App.java
@@ -1,7 +1,10 @@
 package io.hexlet.example;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+	System.out.println(StringUtils.capitalize("hexlet"));
     }
 }
